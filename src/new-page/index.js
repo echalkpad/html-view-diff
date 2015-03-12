8342abebb62638f45bac9c7ac033d44e43761d10
valaxy <valaxy@qq.com>
2015-03-12 21:54:25 +0800

    add

--- src/new-page/index.js
+++ src/new-page/index.js
@@ -3,6 +3,8 @@ define(function (require, exports) {
 	var protobuf = require('../sync/protobuf')
 	var SnapshotDom = require('../model/snapshot-dom')
 	var recover = require('../document-grab/recover')
+	var addMask = require('../document-grab/add-mask')
+	var $ = require('jquery')
 
 	exports.init = function () {
 		protobuf.init(function () {
@@ -10,6 +12,11 @@ define(function (require, exports) {
 				var dom = SnapshotDom.fromProtobuf(data)
 				console.log(dom)
 				recover(dom)
+
+				//setTimeout(function () {
+				// test add mask
+				addMask($('embed')[0])
+				//}, 1000)
 			})
 		})
 	}
