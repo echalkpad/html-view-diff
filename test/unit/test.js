91ef3ae0ed1fcb8e90e1e7a905e7f324189a7596
valaxy <valaxy@qq.com>
2015-03-31 13:30:53 +0800

    refactor project files

--- test/unit/test.js
+++
@@ -1,7 +0,0 @@
-define(function (require) {
-	require('../sync/protobuf')
-	require('../document-grab/id-generator')
-	require('../model/element-node-data')
-	require('../model/text-node-data')
-	require('../document-grab/grab')
-})
